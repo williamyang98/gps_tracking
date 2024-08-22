15dfc90481e3662553ddea165653370efc0fe4fb
William Yang <williamyang2017@gmail.com>
2024-08-23 03:43:55 +1000

    Update main view

--- android_app/app/src/main/java/com/example/gps_tracker/MainActivity.kt
+++ android_app/app/src/main/java/com/example/gps_tracker/MainActivity.kt
@@ -21,7 +21,6 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.core.app.ActivityCompat
 import com.android.volley.NetworkResponse
@@ -32,13 +31,12 @@ import com.android.volley.toolbox.StringRequest
 import com.android.volley.toolbox.Volley
 import com.example.gps_tracker.ui.theme.Gps_trackerTheme
 import com.google.android.gms.location.FusedLocationProviderClient
-import com.google.android.gms.location.LocationRequest
-import com.google.android.gms.location.LocationRequest.*
 import com.google.android.gms.location.LocationServices
 import com.google.android.gms.location.Priority
 import java.nio.ByteBuffer
 import java.nio.ByteOrder
 import java.time.LocalDateTime
+import java.time.ZoneOffset
 
 class BinaryRequest(
     method: Int, url: String,
@@ -66,6 +64,7 @@ class MainActivity : ComponentActivity() {
     private var location: Location? = null;
     private var statusCode: Int? = null;
     private var responseBody: String? = null;
+    private var responseTimestamp: LocalDateTime? = null;
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -83,52 +82,64 @@ class MainActivity : ComponentActivity() {
             ActivityCompat.requestPermissions(this, arrayOf(Manifest.permission.ACCESS_COARSE_LOCATION), 1);
         }
 
-        // fusedLocationClient.getCurrentLocation(Priority.PRIORITY_HIGH_ACCURACY, null).addOnSuccessListener {
-        fusedLocationClient.lastLocation.addOnSuccessListener { location ->
+        fusedLocationClient.getCurrentLocation(Priority.PRIORITY_HIGH_ACCURACY, null).addOnSuccessListener { location ->
             if (location != null) {
                 this.location = location;
                 this.locationTimestamp = LocalDateTime.now();
-                val unix_time_stamp = Math.round(System.currentTimeMillis() / 1000.0).toInt();
-                val user_id: Int = 0;
-
-                val buffer = ByteBuffer.allocate(4 + 4 + 8 + 8 + 8);
-                buffer.order(ByteOrder.LITTLE_ENDIAN);
-                buffer.putInt(user_id);
-                buffer.putInt(unix_time_stamp);
-                buffer.putDouble(location.latitude);
-                buffer.putDouble(location.longitude);
-                buffer.putDouble(location.altitude);
-
-                val url = "https://australia-southeast1-gps-tracking-433211.cloudfunctions.net/post-gps";
-                var request = BinaryRequest(
-                    Request.Method.POST, url, buffer.array(),
-                    { statusCode ->
-                        this.statusCode = statusCode;
-                        this.renderView();
-                    },
-                    { response ->
-                        this.responseBody = response;
-                        this.renderView();
-                    },
-                    { error ->
-                        error.networkResponse?.let { response ->
-                            this.statusCode = response.statusCode;
-                            this.responseBody = String(response.data, Charsets.UTF_8);
-                            this.renderView();
-                        } ?: run {
-                            this.statusCode = null;
-                            this.responseBody = error.message;
-                            this.renderView();
-                        }
-                    }
-                );
-                requestQueue.add(request);
-                this.renderView();
+                this.postGPS();
             } else {
                 this.location = null;
                 this.locationTimestamp = LocalDateTime.now();
+                this.postGPS();
             }
+            this.renderView();
         }
+    }
+
+    private fun postGPS() {
+        val location = this.location;
+        val locationTimestamp = this.locationTimestamp;
+        if (location == null || locationTimestamp == null) {
+            return;
+        }
+        val unixTimeStamp = locationTimestamp.atZone(ZoneOffset.UTC).toEpochSecond();
+        val user_id: Int = 0;
+        val buffer = ByteBuffer.allocate(4 + 4 + 8 + 8 + 8);
+        buffer.order(ByteOrder.LITTLE_ENDIAN);
+        buffer.putInt(user_id);
+        buffer.putInt(unixTimeStamp.toInt());
+        buffer.putDouble(location.latitude);
+        buffer.putDouble(location.longitude);
+        buffer.putDouble(location.altitude);
+
+        val url = "https://australia-southeast1-gps-tracking-433211.cloudfunctions.net/post-gps";
+        var request = BinaryRequest(
+            Request.Method.POST, url, buffer.array(),
+            { statusCode ->
+                this.statusCode = statusCode;
+                this.responseTimestamp = LocalDateTime.now();
+                this.renderView();
+            },
+            { response ->
+                this.responseBody = response;
+                this.responseTimestamp = LocalDateTime.now();
+                this.renderView();
+            },
+            { error ->
+                error.networkResponse?.let { response ->
+                    this.statusCode = response.statusCode;
+                    this.responseBody = String(response.data, Charsets.UTF_8);
+                    this.responseTimestamp = LocalDateTime.now();
+                    this.renderView();
+                } ?: run {
+                    this.statusCode = null;
+                    this.responseBody = error.message;
+                    this.responseTimestamp = LocalDateTime.now();
+                    this.renderView();
+                }
+            }
+        );
+        requestQueue.add(request);
     }
 
     private fun renderView() {
@@ -142,46 +153,50 @@ class MainActivity : ComponentActivity() {
                     color = MaterialTheme.colorScheme.background
                 ) {
                     Column {
+                        Text(text="GPS Location", style=MaterialTheme.typography.headlineSmall)
                         Button(onClick = { refreshLocation() }) {
                             Text(text="Refresh location")
                         }
                         Row {
-                            location?.let { loc ->
-                                LazyColumn {
-                                    item {
-                                        Row {
-                                            TableCell(text = "Longitude", weight = col0)
-                                            TableCell(text = "${loc.longitude}", weight = col1)
-                                        }
-                                    }
-                                    item {
-                                        Row {
-                                            TableCell(text = "Latitude", weight = col0)
-                                            TableCell(text = "${loc.latitude}", weight = col1)
-                                        }
-                                    }
-                                    item {
-                                        Row {
-                                            TableCell(text = "Altitude", weight = col0)
-                                            TableCell(text = "${loc.altitude}", weight = col1)
-                                        }
-                                    }
-                                    item {
-                                        Row {
-                                            TableCell(text = "Time", weight = col0)
-                                            TableCell(
-                                                text = "$locationTimestamp",
-                                                weight = col1
-                                            )
-                                        }
+                            LazyColumn {
+                                item {
+                                    Row {
+                                        TableCell(text="Time", weight=col0)
+                                        TableCell(text="$locationTimestamp", weight=col1)
                                     }
                                 }
-                            } ?: run {
-                                Text(text = "No GPS location")
+                                item {
+                                    Row {
+                                        TableCell(text="Longitude", weight=col0)
+                                        TableCell(text="${location?.longitude}", weight=col1)
+                                    }
+                                }
+                                item {
+                                    Row {
+                                        TableCell(text="Latitude", weight=col0)
+                                        TableCell(text="${location?.latitude}", weight=col1)
+                                    }
+                                }
+                                item {
+                                    Row {
+                                        TableCell(text="Altitude", weight=col0)
+                                        TableCell(text="${location?.altitude}", weight=col1)
+                                    }
+                                }
                             }
+                        }
+                        Text(text="Server Status", style=MaterialTheme.typography.headlineSmall)
+                        Button(onClick = { postGPS() }) {
+                            Text(text="Submit GPS")
                         }
                         Row {
                             LazyColumn {
+                                item {
+                                    Row {
+                                        TableCell(text="Time", weight=col0)
+                                        TableCell(text="$responseTimestamp", weight=col1)
+                                    }
+                                }
                                 item {
                                     Row {
                                         TableCell(text="Status Code", weight=col0)
@@ -201,6 +216,11 @@ class MainActivity : ComponentActivity() {
             }
         }
     }
+
+    @Composable
+    private fun getView() {
+
+    }
 }
 
 @Composable
@@ -209,7 +229,7 @@ fun RowScope.TableCell(
     weight: Float
 ) {
     Text(
-        text = text,
+        text=text,
         Modifier
             .border(1.dp, Color.Black)
             .weight(weight)
